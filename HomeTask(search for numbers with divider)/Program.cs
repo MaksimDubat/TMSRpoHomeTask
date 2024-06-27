af58031b031643d87fa171d907f14ffa1e1ba79e
dubatovka.maksim <dubatovvka.maksim@gmail.com>
2024-06-27 08:32:46 +0300

    deletedadded input into the list

--- HomeTask(search for numbers with divider)/Program.cs
+++ HomeTask(search for numbers with divider)/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Home task!")
+           
+            Console.WriteLine("amount of numbers");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>(amount);
+            Console.WriteLine("put the nambers");
+            for (int i = 0; i < amount; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                list.Add(number);
+            }
         }
     }
 }
